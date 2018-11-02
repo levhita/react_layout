f0215f1714a6c0873b598921998ebbf76246dadf
Levhita <levhita@gmail.com>
2018-11-01 21:30:30 -0600

    Moved posts to own file

--- src/Posts.js
+++ src/Posts.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 // import './Posts.css';
 import Post from './Post';
+import postsJson from './postsJson';
 
 class Posts extends Component {
+  
   constructor(props){
     super(props);
     
     this.state = {
-      posts: [
-        {
-          title: "DMV costume",
-          img:"https://img-9gag-fun.9cache.com/photo/apm0YLE_460s.jpg"
-        },
-        {
-          title: "Small human being a bro!",
-          img: "https://img-9gag-fun.9cache.com/photo/arG4Bj6_460s.jpg"
-        },
-        {
-          title: "Happy Halloween",
-          img: "https://img-9gag-fun.9cache.com/photo/a2ZYbdd_460s.jpg"
-        }
-      ]
+      posts: postsJson
     };
   
   }
